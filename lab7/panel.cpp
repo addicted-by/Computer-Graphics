447ecaad67efdca3816c6e2e7499f9a308b07d84
interference-82 <58998912+interference-82@users.noreply.github.com>
2021-02-05 20:20:51 +0300

    labs

--- lab7/panel.cpp
+++
@@ -1,6 +0,0 @@
-#include "panel.h"
-
-panel::panel(QWidget *parent) : QWidget(parent)
-{
-
-}
--- lab7_1/bezie.cpp
+++
@@ -1,7 +0,0 @@
-#include "bezie.h"
-#include <QWidget>
-
-bezie::bezie(QWidget *parent) : QWidget(parent)
-{
-
-}
